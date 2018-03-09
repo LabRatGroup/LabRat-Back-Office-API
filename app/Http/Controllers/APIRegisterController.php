6191dfce664ef79f3dc18a8f00a08d6023cba33f
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-09 10:35:57 +0100

    Implemented register request class for validation

--- app/Http/Controllers/APIRegisterController.php
+++ app/Http/Controllers/APIRegisterController.php
@@ -2,29 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\APIRegisterRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class APIRegisterController extends Controller
 {
-    public function register(Request $request)
+    public function register(APIRegisterRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email'    => 'required|string|email|max:255|unique:users',
-            'name'     => 'required',
-            'password' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors());
-        }
+//        $validator = Validator::make($request->all(), [
+//            'email'    => 'required|string|email|max:255|unique:users',
+//            'name'     => 'required',
+//            'password' => 'required',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return response()->json($validator->errors(), HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
+//        }
+
         User::create([
             'name'     => $request->get('name'),
             'email'    => $request->get('email'),
             'password' => bcrypt($request->get('password')),
         ]);
+
         $user = User::first();
         $token = JWTAuth::fromUser($user);
 
