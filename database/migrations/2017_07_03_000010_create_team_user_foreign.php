c97a1db885bd3311d37f21f54bf996a197d96378
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-18 20:33:14 +0100

    Change relation name for a more generic identifier

--- database/migrations/2017_07_03_000010_create_team_user_foreign.php
+++ database/migrations/2017_07_03_000010_create_team_user_foreign.php
@@ -9,7 +9,7 @@ class CreateTeamUserForeign extends Migration
     public function up()
     {
         Schema::table('team_user', function (Blueprint $table) {
-            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('item_id')->references('id')->on('teams')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
@@ -18,7 +18,7 @@ class CreateTeamUserForeign extends Migration
     public function down()
     {
         Schema::table('team_user', function (Blueprint $table) {
-            $table->dropForeign(['team_id']);
+            $table->dropForeign(['item_id']);
             $table->dropForeign(['user_id']);
             $table->dropForeign(['role_id']);
         });
