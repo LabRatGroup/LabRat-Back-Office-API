b87cfda15f09e881e68a76bf1c063324a021d16c
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-18 20:24:16 +0100

    Added team migration files

---
+++ database/migrations/2017_07_03_000010_create_team_user_foreign.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamUserForeign extends Migration
+{
+    public function up()
+    {
+        Schema::table('team_user', function (Blueprint $table) {
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('team_user', function (Blueprint $table) {
+            $table->dropForeign(['team_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['role_id']);
+        });
+    }
+}
