c5c241b40d15481cd2e54e6d157b3e149361fee8
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-22 20:59:11 +0100

    Update project completed

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -1,13 +1,11 @@
 <?php
 
+use App\Models\Project;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Project::class, function (Faker $faker) {
     return [
         'title'           => $faker->sentence(rand(2, 3)),
         'description'     => $faker->sentence(20),
-        'token'      => str_random(Project::ITEM_TOKEN_LENGTH),
-        'created_at'      => $faker->date(),
-        'updated_at'      => $faker->date(),
     ];
 });
