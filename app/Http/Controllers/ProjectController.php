32659264fa8530f7ed175387dee3c61eff31a4b2
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-22 16:29:19 +0100

    Added apicontroller extension to project controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ApiController;
 
-class ProjectController extends Controller
+class ProjectController extends ApiController
 {
     //
 }
