45798fa2d461a74ca030a40dd7168ee3c94d1be1
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-25 19:46:36 +0200

    Corrected table name

--- database/migrations/2018_03_25_120051_create_ml_models_table.php
+++ database/migrations/2018_03_25_120051_create_ml_models_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMlModelTable extends Migration
+class CreateMlModelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMlModelTable extends Migration
      */
     public function up()
     {
-        Schema::create('ml_model', function (Blueprint $table) {
+        Schema::create('ml_models', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title')->nullable(false);
             $table->string('description')->nullable();
@@ -23,7 +23,7 @@ class CreateMlModelTable extends Migration
             $table->softDeletes();
         });
 
-        Schema::table('ml_model', function (Blueprint $table) {
+        Schema::table('ml_models', function (Blueprint $table) {
             $table->unique('token');
             $table->index('project_id');
         });
