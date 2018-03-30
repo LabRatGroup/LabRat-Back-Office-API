294df5cafedf2ee88f926b4cdeaadbf10108eda8
Julio M. Fernandez <jfernandez74@gmail.com>
2018-03-30 21:32:46 +0200

    Fix algorithm params migration field

--- database/migrations/2018_03_25_191341_create_ml_algorithm_params.php
+++ database/migrations/2018_03_25_191341_create_ml_algorithm_params.php
@@ -21,7 +21,7 @@ class CreateMlAlgorithmParams extends Migration
             $table->float('low_range')->nullable(true);
             $table->float('high_range')->nullable(true);
             $table->float('step')->nullable(true);
-            $table->float('classType')->nullable(false);
+            $table->string('classType')->nullable(false);
             $table->float('options')->nullable(true);
             $table->unsignedInteger('ml_algorithm_id')->nullable(true);
             $table->timestamps();
