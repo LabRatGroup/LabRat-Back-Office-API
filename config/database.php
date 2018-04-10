a0ea83134993b60974e815b454b0e0d4eb61f9c2
Julio M. Fernandez <jfernandez74@gmail.com>
2018-04-10 23:36:30 +0200

    Added default db name

--- config/database.php
+++ config/database.php
@@ -82,7 +82,7 @@ return [
             'driver'   => 'mongodb',
             'host'     => env('DB_MONGO_HOST', 'localhost'),
             'port'     => env('DB_MONGO_PORT', 27017),
-            'database' => env('DB_MONGO_DATABASE'),
+            'database' => env('DB_MONGO_DATABASE', 'labrat'),
             'username' => env('DB_MONGO_USERNAME'),
             'password' => env('DB_MONGO_PASSWORD'),
             'options'  => [
