ed6b064e809527dffa9c280e6554c05f2bbdcae8
Julio M. Fernandez <jfernandez74@gmail.com>
2018-04-19 22:40:35 +0200

    Added storage SFTP support

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,23 +45,23 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root'   => storage_path('app'),
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
+            'key'    => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'url'    => env('AWS_URL'),
         ],
 
         'ftp' => [
@@ -79,10 +79,11 @@ return [
         ],
 
         'sftp' => [
-            'driver' => 'sftp',
+            'driver'   => 'sftp',
             'host'     => env('SFTP_HOST', '127.0.0.1'),
             'username' => env('SFTP_USERNAME', 'username'),
             'password' => env('SFTP_PASSWORD', 'password'),
+            'port'     => env('SFTP_PORT', 22),
 
             // Settings for SSH key based authentication...
             // 'privateKey' => '/path/to/privateKey',
