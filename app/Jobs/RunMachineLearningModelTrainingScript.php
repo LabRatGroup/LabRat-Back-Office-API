81887b0f9980e4dc9271525e0912b2ddd34d273b
Julio M. Fernandez <jfernandez74@gmail.com>
2018-04-23 23:11:50 +0200

    Train job call

--- app/Jobs/RunMachineLearningModelTrainingScript.php
+++ app/Jobs/RunMachineLearningModelTrainingScript.php
@@ -3,11 +3,13 @@
 namespace App\Jobs;
 
 use App\Models\MlModelState;
+use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
 
 class RunMachineLearningModelTrainingScript implements ShouldQueue
 {
@@ -33,6 +35,12 @@ class RunMachineLearningModelTrainingScript implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $token = $this->state->trainingData->token;
+        Log::info('Launching model training job for data ' . $token);
+        //$token = 'OEl8Wev85Tmb53FIWUDhI8lgA';
+        $client = new Client();
+        $res = $client->get('http://127.0.0.1:3000/train/' . $token);
+        echo $res->getStatusCode(); // 200
+        echo $res->getBody(); // { "type": "User", ....
     }
 }
