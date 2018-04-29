e70b1952600c3f679de4cfc9037f5330dcf65887
Julio M. Fernandez <jfernandez74@gmail.com>
2018-04-29 19:32:13 +0200

    Removed global seeder from app seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         $this->call(MlAlgorithmSeeder::class);
         $this->call(MlAlgorithmParamSeeder::class);
-        $this->call(MlGlobalSeeder::class);
     }
 }
