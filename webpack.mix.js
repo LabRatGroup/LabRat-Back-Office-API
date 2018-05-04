3d7fff2fa3a9bd3cf648b1b0f75fb21095eaf05e
Julio M. Fernandez <jfernandez74@gmail.com>
2018-05-04 22:58:33 +0200

    JS setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.browserSync('www.labrat.dev');
+// mix.browserSync('www.labrat.dev');
+mix.browserSync({proxy: 'localhost:8000'});
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
